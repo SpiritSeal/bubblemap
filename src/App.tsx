967df8df20c7fc801d7a77a6ff2422ad54207354
epodol <34190213+epodol@users.noreply.github.com>
2022-06-05 10:35:46 -0700

    Basic functional d3 in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import MindMap from './MindMap';
 
-const App = () => {
-  const a = 'Mind Map';
-
-  return <h1>{a}</h1>;
-};
+const App = () => (
+  <div style={{ margin: 0, padding: 0 }}>
+    <MindMap />
+  </div>
+);
 
 export default App;
