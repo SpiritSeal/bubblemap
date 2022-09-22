cef348f659434b5041579e35c230a3584d6e436f
Saketh Reddy <45369057+SpiritSeal@users.noreply.github.com>
2022-09-22 10:30:17 -0700

    Standardize Idea Gen Function (#46)
    * Basic Datamuse boilerplate
    
    * Datamuse working, ft. default bad https library
    
    * Standardized Formatting of Data and Text
    
    * no-console
    
    * fixed eslint and prettier issues

--- functions/src/ai/ai.ts
+++ functions/src/ai/ai.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as functions from 'firebase-functions';
 import { Configuration, OpenAIApi } from 'openai';
+
+import * as https from 'https';
 
 // // Read API key from ./secrets/openai_key.secret file and save it to a variable
 const openai_key = process.env.OPENAI_SECRET;
@@ -12,12 +15,49 @@ const openai = new OpenAIApi(configuration);
 async function genIdeaOAI(text: string) {
   const response = await openai.createCompletion({
     model: 'text-davinci-002',
-    prompt: `Single word related to ${text}`,
+    prompt: `Single word or phrase related to ${text}`,
     temperature: 0.5,
     max_tokens: 6,
-    n: 1,
+    n: 3,
   });
-  return response.data;
+  return formatIdeaOAI(response.data);
+}
+
+// Request https://api.datamuse.com/words?rel_trg=cow
+async function genIdeaDM(text: string) {
+  const url = `https://api.datamuse.com/words?rel_trg=${text}&max=3`;
+  const response = await new Promise((resolve, reject) => {
+    https
+      .get(url, (res: any) => {
+        let data = '';
+        res.on('data', (chunk: string) => {
+          data += chunk;
+        });
+        res.on('end', () => {
+          resolve(JSON.parse(data));
+        });
+      })
+      .on('error', (err: any) => {
+        reject(err);
+      });
+  });
+  // console.log(response);
+  return formatIdeaDM(response);
+  // console.log(data);
+  // return "test";
+}
+
+function formatIdeaDM(idea: any) {
+  // builds an array of strings from the Datamuse response
+  const ideas = idea.map((item: any) => item.word);
+  return ideas;
+}
+
+function formatIdeaOAI(idea: any) {
+  // builds an array of strings from the OpenAI response
+  // trim the whitespace and lowercase all the strings
+  const ideas = idea.choices.map((item: any) => item.text.trim().toLowerCase());
+  return ideas;
 }
 
 const ai = functions
@@ -25,12 +65,22 @@ const ai = functions
   .region('us-west2')
   .https.onCall(async (data) => {
     const result = await genIdeaOAI(data.data);
-    if (!result.choices) {
-      throw new functions.https.HttpsError('internal', 'No idea found');
-    }
+    // console.log("OpenAI: \n"+result);
+    const resultDM = await genIdeaDM(data.data);
+    // console.log("Datamuse: \n"+resultDM);
     return {
-      idea: result.choices[0].text,
+      idea: {
+        openai: result,
+        datamuse: resultDM,
+      },
     };
+    // const result = await genIdeaOAI(data.data);
+    // if (!result.choices) {
+    //   throw new functions.https.HttpsError('internal', 'No idea found');
+    // }
+    // return {
+    //   idea: result.choices[0].text,
+    // };
   });
 
 export default ai;
