0b6a516c3158d9f47d03c3ecc99cff229c6413e8
Saketh Reddy <yssaketh@gmail.com>
2022-10-03 21:01:05 -0700

    Selected Node, Variable Disambiguation

--- src/pages/MindMap/Bubble.tsx
+++ src/pages/MindMap/Bubble.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SimulationNodeDatum } from 'd3-force';
-import { node as nodeType } from '../../types';
+import { localNode as nodeType } from '../../types';
+// import './Bubble.css';
 
 const radius = 15;
 const lineHeight = 15.5;
@@ -25,10 +26,20 @@ function wordsGenerator(text: string) {
 
 const Bubble = ({
   node,
+  dragging,
   selected,
+  setSelectedNode,
+  mouseDown,
+  setMouseDown,
+  downMouseCoords,
 }: {
   node: SimulationNodeDatum & nodeType;
+  dragging: boolean;
   selected: boolean;
+  setSelectedNode: (node: SimulationNodeDatum & nodeType) => void;
+  mouseDown: boolean;
+  setMouseDown: (mouseDown: boolean) => void;
+  downMouseCoords: { x: number; y: number };
 }) => {
   // Prepare the text
   const { text } = node;
@@ -66,18 +77,7 @@ const Bubble = ({
     }
     return tradius;
   };
-  // Generate metadata text that will go in the section underneath the main text
-  // const subLines = (function subLines() {
-  //   const subLinesTemp = [];
-  //   const createdBy = 'Saketh Reddy [@spyre]';
-  //   const createdOn = '5 minutes ago';
-  //   subLinesTemp.push(createdBy);
-  //   subLinesTemp.push(createdOn);
-  //   subLinesTemp.push(`Idea #${node.id}`);
-  //   return subLinesTemp;
-  // })();
 
-  // Return Statement
   return (
     <g
       className="bubble"
@@ -87,16 +87,33 @@ const Bubble = ({
       style={
         node.id === 0
           ? { cursor: 'no-drop' }
-          : { cursor: selected ? 'grabbing' : 'grab' }
+          : { cursor: dragging ? 'grabbing' : 'grab' }
       }
+      // onClick set selectedNode and console.log the node
+      onClick={(e) => {
+        if (mouseDown) {
+          setMouseDown(false);
+
+          if (
+            downMouseCoords.x === e.clientX &&
+            downMouseCoords.y === e.clientY
+          ) {
+            setSelectedNode(node);
+          }
+        }
+        // console.log(e);
+        // setSelectedNode(node);
+        // prevent the click from propagating to the svg
+        e.stopPropagation();
+      }}
     >
       <circle
         cx={radius}
         cy={radius}
         r={radius}
         fill={node.id === 0 ? 'lightblue' : '#e3eeff'}
-        stroke="black"
-        strokeWidth="0.5"
+        stroke={selected ? 'pink' : 'black'}
+        strokeWidth={selected ? '7' : '.5'}
       />
       {/* print the main text in the bubble */}
       {/* main text uses https://observablehq.com/@mbostock/fit-text-to-circle */}
@@ -120,57 +137,6 @@ const Bubble = ({
           </tspan>
         ))}
       </text>
-
-      {/* <g className="bottom_region">
-        <path
-          d="M27 24C27 24 23 30 15 30C7 30 3 24 3 24C3 24 7.1318 23 15.0478 23C22.9638 23 27 24 27 24Z"
-          fill="white"
-          stroke="black"
-          strokeWidth="0.5"
-        />
-        <text
-          x={radius}
-          y="25.5"
-          textAnchor="middle"
-          dominantBaseline="auto"
-          fontStyle="italic"
-          // style={{ fontSize: '0.8rem' }}
-          // force text to fit in the region
-          style={{ fontSize: '0.12rem' }}
-        >
-          {subLines.map((line, i) => (
-            <tspan
-              // eslint-disable-next-line react/no-array-index-key
-              key={i}
-              x={radius}
-              dy={i === 0 ? 0 : subLineHeight}
-            >
-              {line}
-            </tspan>
-          ))}
-        </text>
-      </g>
-      <g
-        className="top_region"
-        style={{ cursor: 'pointer' }}
-        onClick={(e) => {
-          // print the node id
-          // eslint-disable-next-line no-console
-          console.log(node.id);
-          e.stopPropagation();
-        }}
-      >
-        <path
-          d="M20 3.00095C20 4.00189 20 5 15 5C10 5 10 4.00189 10 3.00095C9.99999 2 9.99998 1.00379 15 1.00189C20 1 20 2 20 3.00095Z"
-          fill="white"
-        />
-        <path
-          d="M11 2C11 2 18.8225 2.00058 19 2M11 3.02521C11 3.02521 18.8225 3.02579 19 3.02521M11 4C11 4 18.8225 4.00058 19 4"
-          stroke="black"
-          strokeWidth="0.2"
-          strokeLinecap="round"
-        />
-      </g> */}
     </g>
   );
 };
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,16 @@
 import { Timestamp } from 'firebase/firestore';
 
+import { SimulationNodeDatum } from 'd3-force';
+
 export interface node {
   id: number;
   text: string;
   parent: number;
+}
+
+export interface localNode extends node, SimulationNodeDatum {
+  children?: number[];
+  selected?: boolean;
 }
 
 export interface MindMap {
