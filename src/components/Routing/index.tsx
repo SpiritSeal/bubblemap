7c6771c73ba40509958c5fd0e4ad8b630e12bfea
Saketh Reddy <yssaketh@gmail.com>
2022-10-05 13:19:15 -0700

    Begin Home Page, overhaul routing

--- src/components/Routing/index.tsx
+++ src/components/Routing/index.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MindMap from '../../pages/MindMap';
+import HomePage from '../../pages/HomePage';
 
-const Routing = () => <MindMap mindmapID="PxICnzGAskSEQXxkCIL4" />;
+// Create a new component which we can use to route pages
+const Routing = () => (
+  // /mindmap takes you to the mindmap page
+  // / takes you to the homepage
+  <Router>
+    <Routes>
+      <Route
+        path="/mindmap"
+        element={<MindMap mindmapID="PxICnzGAskSEQXxkCIL4" />}
+      />
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  </Router>
+);
+// const Routing = () => <MindMap mindmapID="PxICnzGAskSEQXxkCIL4" />;
 
 export default Routing;
---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage = () => {
+  // Build a basic homepage
+  const one = 1;
+  //   eslint-disable-next-line
+  console.log(one);
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
